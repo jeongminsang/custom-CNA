30244a40e17f8b16faa1d01ff34cc026c344b237
jeongminsang <jong0364@gmail.com>
2024-04-27 18:50:46 +0900

    update nextjs start

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,11 @@
+import "./globals.css";
+
+export const metadata = {
+    title: "Next.js 14 Starter",
+    description:
+        "A Next.js 14 starter project built without using create-next-app.",
+};
+
 const RootLayout = ({ children }) => {
     return (
         <html lang="en">
