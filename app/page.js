6e8f05c03894bfd4846111a5378669b8a349a384
jeongminsang <jong0364@gmail.com>
2024-04-27 18:23:54 +0900

    add app router

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+    title: "Next.js 14 Starter",
+    description:
+        "A Next.js 14 starter project built without using create-next-app.",
+};
+
+const Page = () => {
+    return (
+        <>
+            <h1>Hello, Next.js!</h1>
+        </>
+    );
+};
+
+export default Page;
